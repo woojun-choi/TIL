cfda6e44d0da64b68014874903759e87077d1ed5
cloudy-1014 <cloudy838@icloud.com>
2022-04-19 23:19:03 +0900

    TIL: JS
    Study basics
    - assignment operator, optional parameter, scope
    Clear exercise

---
+++ Java script/tutorial/exercise/exercise_12.js
@@ -0,0 +1,12 @@
+// 여기에 expressMultiplication 함수를 작성해 주세요.
+function expressMultiplication(x, y) {
+  console.log(`${x} * ${y} = ${x * y}`);
+}
+
+// 테스트 코드
+expressMultiplication(3, 4);
+expressMultiplication(3, 2);
+expressMultiplication(7, 5);
+expressMultiplication(8, 9);
+expressMultiplication(5, 5);
+expressMultiplication(9, 9);
--- Java script/tutorial/index.js
+++ Java script/tutorial/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!')
+// console.log('Hello World!');
+
+function hi(a, b, c = 1, d = 2) {
+  console.log(c);
+  console.log(d);
+  //a+b+3
+}
+
+hi(1,2, d = 10);
