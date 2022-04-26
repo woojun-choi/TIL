7b34625c2a4ce3940e34768d9328eea09965a502
cloudy-1014 <cloudy838@icloud.com>
2022-04-26 16:45:59 +0900

    TIL: JS
    Study for...in, etc
    - for...in, date object
    Clear exercises

--- Java script/tutorial/index.js
+++ Java script/tutorial/index.js
@@ -6,4 +6,12 @@ function hi(a, b, c = 1, d = 2) {
   //a+b+3
 }
 
-hi(1,2, d = 10);
+// hi(1,2, d = 10);
+
+let tDate = new Date();
+
+// console.log(tDate.getDay());
+console.log(typeof tDate);
+
+console.log(typeof Date(2020,01,01));                  
+console.log(typeof new Date(2020, 01, 01));              
